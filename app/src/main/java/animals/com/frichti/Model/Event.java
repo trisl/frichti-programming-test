80690d53b7f21e6d99cfe5cf1a591febecc34c8a
Tristan Roussel <tristan.roussel@epita.fr>
2017-11-13 12:41:19 +0100

    Create the event model to draw them in the recycler view.

---
+++ app/src/main/java/animals/com/frichti/Model/Event.java
@@ -0,0 +1,31 @@
+package animals.com.frichti.Model;
+
+/**
+ * Created by Tristan on 13/11/2017.
+ */
+
+public class Event {
+    private String title;
+    private int pictureId;
+    
+    public Event(String title, int pictureId) {
+        this.title = title;
+        this.pictureId = pictureId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPictureId() {
+        return pictureId;
+    }
+
+    public void setPictureId(int pictureId) {
+        this.pictureId = pictureId;
+    }
+}
